f7d0dc340f2f25cb816899fa9a3331cc36cb5280
Marcelo Oliveira <marcelo.olisi@gmail.com>
2022-12-02 13:59:44 -0300

    Configuration DependecyInjection and IServiceCollection

--- WorldCup.Infra.Ioc/DependecyInjection.cs
+++ WorldCup.Infra.Ioc/DependecyInjection.cs
@@ -1,6 +1,28 @@
-﻿namespace WorldCup.Infra.Ioc
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WorldCup.Domain.Interfaces;
+using WorldCup.Infra.Data.Context;
+using WorldCup.Infra.Data.Repositories;
+
+namespace WorldCup.Infra.Ioc
 {
-    internal class DependecyInjection
+    public static class DependecyInjection
     {
+
+        public static IServiceCollection AddInfrastruture(this IServiceCollection services, 
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseSqlServer(configuration.GetConnectionString("WorldCupConnection"), b => b
+                .MigrationsAssembly(typeof(ApplicationDbContext)
+                .Assembly.FullName)));
+
+            services.AddScoped<ICupTitleRepository, CupTitleRepository>();
+            services.AddScoped<IFootballClubRepository, FootballClubRepository>();
+
+            return services;
+
+        }
     }
 }
