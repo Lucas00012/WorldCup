d2221ce34b46dbe466ce4c2f2c5eaccb13163255
Marcelo Oliveira <marcelo.olisi@gmail.com>
2022-12-02 02:11:33 -0300

    Create validation

--- WorldCup.Domain/Entities/CupTitle.cs
+++ WorldCup.Domain/Entities/CupTitle.cs
@@ -1,20 +1,51 @@
-﻿namespace WorldCup.Domain.Entities
+﻿using WorldCup.Domain.Validation;
+
+namespace WorldCup.Domain.Entities
 {
     public sealed class CupTitle
     {
-        public int Id { get; set; }
+        public int Id { get; private set; }
 
-        public int CupYear { get; set; }
+        public int CupYear { get; private set; }
 
-        public string Location { get; set; }
-
-        public string Opponent { get; set; }
+        public string Location { get; private set; }
 
         public int FootballClubId { get; set; } //Foreign Key
 
         public FootballClub FootballClub { get; set; }
 
+        private void ValidateDomain(int cupYear, string location)
+        {
+            DomainExceptionValidation.When(CupYear < 1930, "Invalid year for the FIFA World Cup! Please enter a valid year (It starts from 1930)");
 
+            DomainExceptionValidation.When(string.IsNullOrEmpty(location),"Invalid location! Location is required");
+            DomainExceptionValidation.When(location.Length <3, "Invalid location, too short, minimum 3 characterss");
+
+            CupYear = cupYear;
+            Location = location;
+
+        }
+
+        public void Update(int cupYear, string location, int footballClubId)
+        {
+            ValidateDomain(cupYear, location);
+
+            FootballClubId = footballClubId;
+
+        }
+
+        public CupTitle(int cupYear, string location)
+        {
+            ValidateDomain(cupYear, location);
+        }
+
+        public CupTitle(int id, int cupYear, string location)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid ID");
+            Id = id;
+
+            ValidateDomain(cupYear, location);
+        }
 
 
     }
--- WorldCup.Domain/Entities/FootballClub.cs
+++ WorldCup.Domain/Entities/FootballClub.cs
@@ -1,14 +1,44 @@
-﻿namespace WorldCup.Domain.Entities
+﻿using WorldCup.Domain.Validation;
+
+namespace WorldCup.Domain.Entities
 {
     public sealed class FootballClub
     {
-        public int Id { get; set; }
+        public int Id { get; private set; }
 
-        public string Country { get; set; }
-        public string Image { get; set; }
+        public string Country { get; private set; }
+        public string Image { get; private set; }
 
         public ICollection<CupTitle> CupTitles { get; set; }
 
+
+        public FootballClub(string country, string image)
+        {
+            ValidateDomain(country, image);
+        }
+
+        public FootballClub(int id, string country, string image)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid ID");
+            Id = id;
+            ValidateDomain(country, image);
+        }
+
+        private void ValidateDomain(string country, string image)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(country), "Invalid country! Country is required");
+            DomainExceptionValidation.When(country.Length < 3, "Invalid country, too short, minimum 3 characters");
+
+            DomainExceptionValidation.When(image.Length > 300, "Path of image too long");
+
+            Country = country;
+            Image = image;
+        }
+
+        public void Update(string country, string image)
+        {
+            ValidateDomain(country, image);
+        }
 
 
 
