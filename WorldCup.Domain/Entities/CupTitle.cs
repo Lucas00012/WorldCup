7a5d3d4a63088bf71cd13e83cf0d9396744e4c5e
Marcelo Oliveira <marcelo.olisi@gmail.com>
2022-12-02 01:27:46 -0300

    Sealed Classes

---
+++ WorldCup.Domain/Entities/CupTitle.cs
@@ -0,0 +1,21 @@
+﻿namespace WorldCup.Domain.Entities
+{
+    public sealed class CupTitle
+    {
+        public int Id { get; set; }
+
+        public int CupYear { get; set; }
+
+        public string Location { get; set; }
+
+        public string Opponent { get; set; }
+
+        public int FootballClubId { get; set; } //Foreign Key
+
+        public FootballClub FootballClub { get; set; }
+
+
+
+
+    }
+}
---
+++ WorldCup.Domain/Entities/FootballClub.cs
@@ -0,0 +1,16 @@
+﻿namespace WorldCup.Domain.Entities
+{
+    public sealed class FootballClub
+    {
+        public int Id { get; set; }
+
+        public string Country { get; set; }
+        public string Image { get; set; }
+
+        public ICollection<CupTitle> CupTitles { get; set; }
+
+
+
+
+    }
+}
