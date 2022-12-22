b34eed9e12f41852e9b85a463552f03ac878385f
Marcelo Oliveira <marcelo.olisi@gmail.com>
2022-12-22 16:05:55 -0300

    Create class AutoMapper

---
+++ WorldCup.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using WorldCup.Application.DTOs;
+using WorldCup.Domain.Entities;
+
+namespace WorldCup.Application.Mappings
+{
+    public class DomainToDTOMappingProfile :Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<CupTitle, CupTitleDTO>().ReverseMap();
+            CreateMap<FootballClub, FootballClubDTO>().ReverseMap();
+        }
+
+
+
+    }
+}
