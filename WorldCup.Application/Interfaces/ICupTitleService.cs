2f36160a568eddba55d23bf22b23d7694ab17f0c
Marcelo Oliveira <marcelo.olisi@gmail.com>
2022-12-22 18:41:49 -0300

    Implementation CupTitleService in Package Application

---
+++ WorldCup.Application/Interfaces/ICupTitleService.cs
@@ -0,0 +1,18 @@
+﻿using WorldCup.Application.DTOs;
+
+namespace WorldCup.Application.Interfaces
+{
+    public interface ICupTitleService
+    {
+
+        Task<IEnumerable<CupTitleDTO>> GetCupTitles();
+        Task<CupTitleDTO> GetById(int? id);
+
+        Task Add(CupTitleDTO cupTitleDto);
+
+        Task Update(CupTitleDTO cupTitleDto);
+
+        Task Remove(int? id );
+
+    }
+}
---
+++ WorldCup.Application/Services/CupTitleService.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using WorldCup.Application.DTOs;
+using WorldCup.Application.Interfaces;
+using WorldCup.Domain.Entities;
+using WorldCup.Domain.Interfaces;
+
+namespace WorldCup.Application.Services
+{
+    public class CupTitleService : ICupTitleService
+    {
+        private ICupTitleRepository _cupTitleRepository;
+
+        private readonly IMapper _mapper;
+
+        public CupTitleService(ICupTitleRepository cupTitleRepository,IMapper mapper)
+        {
+            _cupTitleRepository = cupTitleRepository;
+            _mapper= mapper;
+        }
+
+
+        public async Task<CupTitleDTO> GetById(int? id)
+        {
+            var cupTitleEntity = await _cupTitleRepository.GetCupTitleByIdAsync(id);
+
+            return _mapper.Map<CupTitleDTO>(cupTitleEntity);
+        }
+
+        public async Task<IEnumerable<CupTitleDTO>> GetCupTitles()
+        {
+            var cupTitleEntities = await _cupTitleRepository.GetCupTitlesAsync();
+
+            return _mapper.Map<IEnumerable<CupTitleDTO>>(cupTitleEntities);
+        }
+
+
+        public async Task Add(CupTitleDTO cupTitleDto)
+        {
+
+            var cupTitleEntity = _mapper.Map<CupTitle>(cupTitleDto);
+            await _cupTitleRepository.CreateAsync(cupTitleEntity);
+
+        }
+
+        public async Task Update(CupTitleDTO cupTitleDto)
+        {
+            var cupTitleEntity = _mapper.Map<CupTitle>(cupTitleDto);
+
+            await _cupTitleRepository.UpdateAsync(cupTitleEntity);
+
+        }
+        public async Task Remove(int? id)
+        {
+            var cupTitleEntity = _cupTitleRepository.GetCupTitleByIdAsync(id).Result;
+            await _cupTitleRepository.RemoveAsync(cupTitleEntity);
+
+        }
+
+
+    }
+}
