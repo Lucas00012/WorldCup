bf22b08c2e06fa0097292a8882457f905288f60b
Marcelo Oliveira <marcelo.olisi@gmail.com>
2022-12-26 13:32:25 -0300

    API build start

---
+++ WorldCup.WebUI/Controllers/CupTitleController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorldCup.WebUI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CupTitleController : ControllerBase
+    {
+        private readonly ILogger<CupTitleController> _logger;
+
+        public CupTitleController(ILogger<CupTitleController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return (Ok()); 
+        }
+
+        [HttpPost]
+        public IActionResult Create()
+        {
+            return CreatedAtAction(nameof(Create),"");
+        }
+
+        [HttpPut]
+        public IActionResult Update()
+        {
+            return NoContent();
+        }
+
+        [HttpPatch]
+        public IActionResult Updat()
+        {
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
