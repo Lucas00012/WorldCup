505a002561b858143a46313428e4be7316dd4901
Marcelo Oliveira <marcelo.olisi@gmail.com>
2022-12-27 19:00:14 -0300

    Create models and service file

---
+++ WorldCup.WebUI/ClientApp/src/model/cupTitle.ts
@@ -0,0 +1,6 @@
+export class CupTitle {
+  cupTitleId: string | undefined;
+  cupYear: number | undefined;
+  location: string | undefined;
+  footballClub: string | undefined ;
+}
---
+++ WorldCup.WebUI/ClientApp/src/model/footballClub.ts
@@ -0,0 +1,7 @@
+import { CupTitle } from "./cupTitle";
+
+export class FootballClub {
+  country: string | undefined;
+  image: string | undefined;
+  CupTitles: CupTitle | undefined;
+}
